4b4a26a1fb76ebbe66b965791ffad1b0cdf94cf8
Rodrigo Souza <roddsouza@hotmail.com>
2023-05-12 17:41:21 -0400

    file upload for block07

---
+++ secureTheVault.js
@@ -0,0 +1,22 @@
+function openVault(){
+
+    //Write the String with the phrase provided
+    let userMessage = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: ";
+
+    //create variables using any mathematical operation to assign the correct value to each variable 
+
+        //value must be 10
+        let a = 2 * 5;
+
+        //value must be 40
+        let b = 120 / 3;
+
+        //value must be 39
+        let c = 7 * 6 - 3;
+
+        //Send out alert on page with message and combination by concatenating the variables
+        alert(userMessage + a + "-" + b + "-" + c);
+}
+
+openVault();
+
